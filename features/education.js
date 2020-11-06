19c5fe2f4b7f176959b3d20457b382abe52fca6a
Catherine <catkimyj@gmail.com>
2020-11-06 18:00:48 -0500

    undo git reset hard

--- features/education.js
+++ features/education.js
@@ -9,15 +9,15 @@ module.exports = function (controller) {
       async (bot, message) => {
         await bot.reply(
           message,
-          `${json.education[0].institution}, ${json.education[0].area}. ${json.education[0].startDate} - ${json.education[0].endDate}`
+          `${json.education[0].institution}, ${json.education[0].area}. ${json.education[0].startDate} - ${json.education[0].startDate}`
         );
 
         await bot.reply(
           message,
-          `${json.education[1].institution}, ${json.education[1].area}. ${json.education[1].startDate} - ${json.education[1].endDate}`
+          `${json.education[1].institution}, ${json.education[1].area}. ${json.education[1].startDate} - ${json.education[1].startDate}`
         );
         await bot.reply(message, {
-          text: `${json.education[2].institution}, ${json.education[2].area}. ${json.education[2].startDate} - ${json.education[2].endDate}`,
+          text: `${json.education[2].institution}, ${json.education[2].area}. ${json.education[2].startDate} - ${json.education[2].startDate}`,
           quick_replies: [
             {
               title: "Basic",
